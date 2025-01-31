6095d94ff6d52dd3a53a371360376979681bc1d5
djwlfdls1 <djwlfdls1@gmail.com>
2025-02-01 00:01:14 +0900

    bac

--- 링크/_res/pre-ㅁ.js
+++
@@ -1,6 +0,0 @@
-$(`.pre-ㅁ`).each(function(){
-    const t=$(this).text();
-    console.log(t);
-    const t_= t.replace(/ㅁ/g,'<span style="visibility: hidden;">ㅁ</span>');
-    $(this).html(t_);
-})
